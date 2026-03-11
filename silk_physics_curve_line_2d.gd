ef36566cabd12fc0fcd1ccb3ed68110e9f4645ce
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2026-03-12 04:35:01 +0900

    update
    update

--- silk_physics_curve_line_2d.gd
+++ silk_physics_curve_line_2d.gd
@@ -47,6 +47,7 @@ signal length_changed(value: float)
 @export var points_rid: Array[RID]
 @export_flags_2d_physics var mask: int = 1
 
+@export_tool_button("Restore", "Node") var _restore: Callable = func() -> void: _create_points(); queue_redraw();
 
 func dist_point_by_point() -> float:
 	return length / float(step + 1)
@@ -63,7 +64,7 @@ func _create_points() -> void:
 	
 	var pnt_dist_step: Vector2 = (start_point.direction_to(end_point) * _양_끝점의_길이()) / (float(step) + 1.)
 	
-	for i: int in range(1, 5, 1):
+	for i: int in range(1, step + 1, 1):
 		var point: Vector2 = start_point + (pnt_dist_step * float(i))
 		points.push_back(point)
 	
@@ -82,35 +83,112 @@ func _enter_tree() -> void:
 	_create_points()
 
 
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_ENTER_TREE:
+			pass
+
+
 func _physics_process(delta: float) -> void:
-	var space_state = get_world_2d().direct_space_state
+	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
 	
 	if !Engine.is_editor_hint() or (test_mode and Engine.is_editor_hint()):
 		if state == State.CAST:
-			for i: int in range(1, points.size(), 1):
-				var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(
-					global_position + points[i],
-					global_position + points[i] + _get_default_gravity(delta),
-					mask,
-				)
-				query.hit_from_inside = true
-				var result: Dictionary = space_state.intersect_ray(
-					query
-				)
+			for i: int in range(1, step + 1, 1):
+				var current_pnt: Vector2 = points[i]
+				var left_pnt: Vector2 = points[i - 1]
+				var right_pnt: Vector2 = points[i + 1]
+				var dist_to_left_pnt: float = current_pnt.distance_squared_to(left_pnt)
+				var dist_to_right_pnt: float = current_pnt.distance_squared_to(right_pnt)
+				var dist_pbp: float = dist_point_by_point()
 				
-				if !result.is_empty():
-					var collide_point: Vector2 = result.position
-					var normal: Vector2 = result.normal
-			
+				var current_position_is_left: bool = i < (step / 2. + 1)
+				var current_position_is_middle: bool = i == step / 2. + 1
+				var current_position_is_right: bool = i > step / 2. + 1
+				
+				var is_current_dist_less_by_left: bool = dist_to_left_pnt < dist_pbp ** 2
+				var is_current_dist_less_by_right: bool = dist_to_right_pnt < dist_pbp ** 2
+				
+				if !_point_is_on_floor(points[i], space_state) and (is_current_dist_less_by_left or is_current_dist_less_by_right):
+					var current_pos: Vector2 = global_position + points[i]
+					var next_pos: Vector2 = current_pos + _get_default_gravity(delta)
+					
+					if current_position_is_left:
+						var direction: Vector2 = current_pos.direction_to(next_pos)
+						if left_pnt.distance_squared_to(next_pos) > dist_pbp ** 2:
+							var _fixed_pos: Vector2 = direction * dist_pbp
+							next_pos = _fixed_pos
+						
+						if right_pnt.distance_squared_to(next_pos) < dist_pbp ** 2:
+							var right_pnt_global_pos: Vector2 = right_pnt + global_position
+							var current_dist: float = right_pnt_global_pos.distance_to(next_pos)
+							direction = right_pnt_global_pos.direction_to(next_pos)
+							var _fixed_pos: Vector2 = direction * (dist_pbp - current_dist)
+							next_pos += _fixed_pos
+					elif current_position_is_right:
+						pass
+					
+					var ray_result: Dictionary = _point_is_colliding(current_pos, next_pos, space_state)
+					
+					if !ray_result.is_empty():
+						var collide_point: Vector2 = ray_result.position
+						var normal: Vector2 = ray_result.normal
+						var reminder: Vector2 = current_pos - next_pos
+						
+					else:
+						points[i] = next_pos
+					
 	
-	queue_redraw()
+		queue_redraw()
+
+# RAYCASTING
+func _point_is_colliding(current_point: Vector2, purposing_position: Vector2, space_state: PhysicsDirectSpaceState2D) -> Dictionary:
+	var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(
+		current_point, purposing_position, mask
+	)
+	query.hit_from_inside = true
+	
+	var result: Dictionary = space_state.intersect_ray( query )
+
+	return result
+	
+
+# POINT CASTING
+func _point_is_on_floor(pnt: Vector2, space_state: PhysicsDirectSpaceState2D) -> bool:
+	var p_query: PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
+	p_query.position = pnt
+	p_query.collide_with_bodies = true
+	p_query.collision_mask = mask
+	var p_result: Array[Dictionary] = space_state.intersect_point(p_query)
+	if !p_result.is_empty():
+		return true
+	
+	return false
 
 
 func _draw() -> void:
+	if points.is_empty(): return
+	
+	#var current_color: Color = Color(0., 0., .15, 1.)
+	
+	var idx: int = 1
+	
 	for point: Vector2 in points:
-		draw_circle(
-			point, 3., Color.WHITE
-		)
+		var is_pined_point: bool = points[0] == point or points[points.size() - 1] == point
+		if is_pined_point:
+			draw_circle(
+				point, 3., Color.WHITE
+			)
+		else:
+			draw_circle(
+				point, 3., Color.BLUE_VIOLET
+			)
+			
+			draw_char(
+				ThemeDB.fallback_font, point, str(idx), 12
+			)
+			
+			idx += 1
 
 #region !! deprecated !! joint 모드를 위해 일시적으로 주석처리하였습니다.
 	#var space: RID = get_world_2d().space
