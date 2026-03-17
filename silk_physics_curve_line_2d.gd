0c3851d2d0f1218e2657d4725094b35fc541476d
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2026-03-17 19:17:35 +0900

    update dialogue manager

--- silk_physics_curve_line_2d.gd
+++ silk_physics_curve_line_2d.gd
@@ -1,6 +1,6 @@
 @tool
 extends Node2D
-class_name SilkPhysicsCurveLine2D
+class_name SilkCurveLine2D
 
 
 @export_custom(PROPERTY_HINT_NONE, "", PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_READ_ONLY)
@@ -25,7 +25,12 @@ var curve: Curve2D = Curve2D.new()
 		#length_changed.emit(value)
 @export var end_point: Vector2 = Vector2(300., 0.)
 @export_range(3, 64, 1) var step: int = 64
-@export var inout_max_ratio: float = PI / 2.
+@export var inout_max_ratio: float = PI / 2.:
+	get:
+		return rad_to_deg(inout_max_ratio)
+	set(value):
+		inout_max_ratio = deg_to_rad(value)
+		
 
 @export var points: PackedVector2Array
 @export_flags_2d_physics var mask: int = 1
@@ -44,31 +49,38 @@ func _create_points() -> void:
 	var left_bottom: Vector2 = Vector2(0., height).rotated(silk_direction.angle())
 	var right_bottom: Vector2 = Vector2(end_point.length(), height).rotated(silk_direction.angle())
 	
+	
 	points.push_back(start_point)
 	points.push_back(left_bottom)
 	points.push_back(right_bottom)
 	points.push_back(end_point)
 	
-	var middle_point: Vector2 = Vector2(end_point.x - start_point.x, right_bottom.y)
-	var dir_start_to_middle_point: Vector2 = start_point.direction_to(middle_point)
-	var dir_end_to_middle_point: Vector2
-
-
-	for point: Vector2 in points:
-		curve.add_point(point)
 	
-	#curve.set_point_out()
+	var middle_point: Vector2 = Vector2((end_point.x - start_point.x) / 2., right_bottom.y)
+	var dir_start_to_middle_point: Vector2 = start_point.direction_to(middle_point)
+	var dir_end_to_middle_point: Vector2 = end_point.direction_to(middle_point)
 
-
- 
+	curve.add_point(start_point)
+	curve.add_point(middle_point)
+	curve.add_point(end_point)
 
 
 func _draw() -> void:
+	var curve_points: PackedVector2Array = []
+	
+	if curve.point_count > 0:
+		for i: int in range(curve.point_count):
+			curve_points.push_back(curve.get_point_position(i))
+	
+	draw_polyline(curve_points, Color.AQUA)
+	
+	
 	if !points.is_empty():
 		draw_polyline(points, Color.WHITE)
 
 
 func _clear_points() -> void:
+	curve.clear_points()
 	points.clear()
 
 
