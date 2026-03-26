f2ea95b5ddedd5283cc491e23e3d174efce47d06
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2026-03-26 12:17:48 +0900

    update
    update

--- glass_2d.gd
+++ glass_2d.gd
@@ -87,7 +87,10 @@ func _notification(what: int) -> void:
 				polygon,
 				[], # Colors
 				[
-					
+					Vector2(0., 0.),
+					Vector2(0., 1.),
+					Vector2(1., 1.),
+					Vector2(1., 0.)
 				],
 				texture_rid
 			)
