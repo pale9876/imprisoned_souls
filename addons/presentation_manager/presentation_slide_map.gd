064b15ada89c55c39ac68370a2c1c46dccd932da
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2026-03-31 02:16:49 +0900

    update
    update

---
+++ addons/presentation_manager/presentation_slide_map.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name PresentationSlideMap
+
+
+@export var map: Dictionary[int, Vector2i]
+
+
+func add_screen(pos: Vector2i) -> void:
+	pass
--- addons/presentation_manager/slide_container.gd
+++ addons/presentation_manager/slide_container.gd
@@ -7,7 +7,23 @@ var screens: Array[Control] = []
 
 
 @export var _slide_tween_curve: Curve = Curve.new()
-@export var _progress: float = 0.
+
+@export_tool_button("Next") var _next_slide: Callable = next
+@export_tool_button("Previous") var _prev_slide: Callable = prev
+
+
+@export var _progress: float = 0.:
+	set(value):
+		_progress = value
+		_progress_changed()
+
+
+var _slide_wait: bool = false
+
+
+func _progress_changed() -> void:
+	for i: int in range(screens.size()):
+		screens[i].global_position.x = global_position.x + ((i % screens.size()) * size.x) - (_progress * size.x)
 
 
 func _notification(what: int) -> void:
@@ -36,3 +52,30 @@ func _update() -> void:
 		screens[i].set_size(self.get_size())
 		screens[i].set_global_position(pos)
 		pos.x += size.x
+
+
+func next() -> void:
+	tween_progress(_progress + 1.)
+
+
+func prev() -> void:
+	tween_progress(_progress - 1.)
+
+
+func tween_progress(final_value: float) -> void:
+	if _slide_wait: return
+	
+	var _tween: Tween = create_tween()
+	
+	_tween.tween_property(
+		self, "_progress", final_value, 1.
+	).set_custom_interpolator(
+		func(value: float) -> float:
+			return _slide_tween_curve.sample_baked(value)
+	)
+	
+	_slide_wait = true
+	
+	await _tween.finished
+	
+	_slide_wait = false
