a3b6215598e2123a930f4927a8a9ceec627949ad
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2026-04-23 01:11:27 +0900

    update
    update

--- chara/character_information.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name CharacterInformation
-
-@export var name: StringName = &""
-@export var hp: int = 100
-@export var attack: float = 1.
-@export var defense: float = 1.
-@export var attack_speed_scale: float = 1.
