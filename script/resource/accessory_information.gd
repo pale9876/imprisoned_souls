52b2b78a040c5c2e8283ea1a039c956d581cbacb
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2026-09-21 22:11:43 +0900

    update

---
+++ script/resource/accessory_information.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name AccessoryInformation
+
+
+@export var name: StringName
+@export_multiline() var description: String
+@export var icon: Texture2D
+@export var texture: Texture2D
+@export var scene: PackedScene
--- script/resource/skill_information.gd
+++ script/resource/skill_information.gd
@@ -2,7 +2,7 @@
 extends Resource
 class_name SkillInformation
 
-
+@export var enable: bool = false
 @export var name: StringName
 @export var icon: Texture
 @export_multiline var description: String
